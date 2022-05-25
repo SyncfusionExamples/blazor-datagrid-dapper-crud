ebb00937d0d5203b30832ec41232edf376af7da2
Joshna Lingala Uthama Reddy Lingala <joshnalingala.u@syncfusion.com>
2022-05-25 11:08:49 +0530

    Updating Samples

---
+++ NET6/Dapper.CRUD/Dapper.CRUD/Data/Bug.cs
@@ -0,0 +1,16 @@
+﻿namespace Dapper.CRUD.Data
+{
+    public class Bug
+    {
+
+        public int Id { get; set; }
+
+        public string Summary { get; set; }
+
+        public string BugPriority { get; set; }
+
+        public string Assignee { get; set; }
+
+        public string BugStatus { get; set; }
+    }
+}
---
+++ NET6/Dapper.CRUD/Dapper.CRUD/Data/BugDataAdaptor.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.Blazor;
+using Syncfusion.Blazor.Data;
+
+namespace Dapper.CRUD.Data
+{
+    public class BugDataAdaptor : DataAdaptor
+    {
+        private BugDataAccessLayer _dataLayer;
+        public BugDataAdaptor(BugDataAccessLayer bugDataAccessLayer)
+        {
+            _dataLayer = bugDataAccessLayer;
+        }
+
+        public override async Task<object> ReadAsync(DataManagerRequest dataManagerRequest, string key = null)
+        {
+            List<Bug> bugs = await _dataLayer.GetBugsAsync();
+            int count = await _dataLayer.GetBugCountAsync();
+            return dataManagerRequest.RequiresCounts ? new DataResult() { Result = bugs, Count = count } : count;
+        }
+
+        public override async Task<object> InsertAsync(DataManager dataManager, object data, string key)
+        {
+            await _dataLayer.AddBugAsync(data as Bug);
+            return data;
+        }
+
+        public override async Task<object> UpdateAsync(DataManager dataManager, object data, string keyField, string key)
+        {
+            await _dataLayer.UpdateBugAsync(data as Bug);
+            return data;
+        }
+
+        public override async Task<object> RemoveAsync(DataManager dataManager, object primaryKeyValue, string keyField, string key)
+        {
+            await _dataLayer.RemoveBugAsync(Convert.ToInt32(primaryKeyValue));
+            return primaryKeyValue;
+        }
+    }
+}
